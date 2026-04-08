81d85a805af50969d871109fb2a3bfc3fc3dab18
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2026-04-08 09:38:50 +0200

    Implement RailMenu

---
+++ RailMenuDemo/Helpers/NavItemIconHelper.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RailMenuDemo.Helpers;
+
+internal class NavItemIconHelper
+{
+    // The icon shown when the item IS selected (accent colour)
+    public static object GetSelectedIcon(DependencyObject obj) =>
+        obj.GetValue(SelectedIconProperty);
+    public static void SetSelectedIcon(DependencyObject obj, object value) =>
+        obj.SetValue(SelectedIconProperty, value);
+    public static readonly DependencyProperty SelectedIconProperty =
+        DependencyProperty.RegisterAttached(
+            "SelectedIcon", typeof(object), typeof(NavItemIconHelper),
+            new PropertyMetadata(null));
+
+    // The icon shown when the item is NOT selected
+    public static object GetUnselectedIcon(DependencyObject obj) =>
+        (object)obj.GetValue(UnselectedIconProperty);
+    public static void SetUnselectedIcon(DependencyObject obj, object value) =>
+        obj.SetValue(UnselectedIconProperty, value);
+    public static readonly DependencyProperty UnselectedIconProperty =
+        DependencyProperty.RegisterAttached(
+            "UnselectedIcon", typeof(object), typeof(NavItemIconHelper),
+            new PropertyMetadata(null));
+
+    // Controls Visibility of the static-icon Viewboxes (Collapsed by default;
+    // set to Visible on items that use the SelectedIcon/UnselectedIcon pair)
+    public static Visibility GetStaticIconVisibility(DependencyObject obj) =>
+        (Visibility)obj.GetValue(StaticIconVisibilityProperty);
+    public static void SetStaticIconVisibility(DependencyObject obj, Visibility value) =>
+        obj.SetValue(StaticIconVisibilityProperty, value);
+    public static readonly DependencyProperty StaticIconVisibilityProperty =
+        DependencyProperty.RegisterAttached(
+            "StaticIconVisibility", typeof(Visibility), typeof(NavItemIconHelper),
+            new PropertyMetadata(Visibility.Collapsed));
+
+    // Optional notification dot on an item
+    public static bool GetShowNotificationDot(DependencyObject obj) =>
+        (bool)obj.GetValue(ShowNotificationDotProperty);
+    public static void SetShowNotificationDot(DependencyObject obj, bool value) =>
+        obj.SetValue(ShowNotificationDotProperty, value);
+    public static readonly DependencyProperty ShowNotificationDotProperty =
+        DependencyProperty.RegisterAttached(
+            "ShowNotificationDot", typeof(bool), typeof(NavItemIconHelper),
+            new PropertyMetadata(false));
+}
--- RailMenuDemo/MainWindow.xaml.cs
+++ RailMenuDemo/MainWindow.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -12,6 +13,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using WinUIEx;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -21,11 +23,61 @@ namespace RailMenuDemo
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainWindow : Window
+    public sealed partial class MainWindow : WindowEx
     {
         public MainWindow()
         {
             InitializeComponent();
+            SetTitleBar();
+        }
+
+        // Handle navigation when a menu item is clicked
+        // Navigation is based on the Tag property of the NavigationViewItem, which is set in the xaml
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            var tag = args.InvokedItemContainer?.Tag?.ToString();
+            switch (tag)
+            {
+                //case "Home": NavFrame.Navigate(typeof(Pages.HomePage)); break;
+                //case "Browse": NavFrame.Navigate(typeof(Pages.BrowsePage)); break;
+                //case "Models": NavFrame.Navigate(typeof(Pages.ModelsPage)); break;
+                //case "Settings": NavFrame.Navigate(typeof(Pages.SettingsPage)); break;
+            }
+        }
+
+        // This is not strictly needed for the menu, but the AI Dev Gallery has child pages that you can navigate to, and without this code the correct menu item would not be highlighted when you navigate back to the main pages, so I added this to keep the correct item highlighted after back-navigation.
+        private void NavFrame_Navigated(object sender, Microsoft.UI.Xaml.Navigation.NavigationEventArgs e)
+        {
+            // Keep the correct item highlighted after back-navigation
+            foreach (var item in NavView.MenuItems.OfType<NavigationViewItem>())
+            {
+                if (item.Tag?.ToString() == GetTagForPage(e.SourcePageType))
+                {
+                    NavView.SelectedItem = item;
+                    break;
+                }
+            }
+        }
+
+        // This method is used to get the tag for a given page type, this is used in the NavFrame_Navigated method to keep the correct item highlighted after back-navigation, you can adjust this method to match your own page types and tags
+        private static string? GetTagForPage(Type page) => page.Name switch
+        {
+            //nameof(Pages.HomePage) => "Home",
+            //nameof(Pages.BrowsePage) => "Browse",
+            //nameof(Pages.ModelsPage) => "Models",
+            //nameof(Pages.SettingsPage) => "Settings",
+            _ => null
+        };
+
+        // Not strictly needed for the menu, but this sets up the title bar with an icon and a title, you can customize this to your liking, or remove it if you don't want a custom title bar
+        private void SetTitleBar()
+        {
+            this.ExtendsContentIntoTitleBar = true;
+            this.SetTitleBar(titleBar);
+            this.AppWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Tall;
+            this.AppWindow.SetIcon("Assets/AppIcon/Icon.ico");
+
+            this.Title = Windows.ApplicationModel.Package.Current.DisplayName;
         }
     }
 }
